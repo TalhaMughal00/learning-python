6059b20059f83e50bf7c4bea6e96e6fce9d707f7
TalhaKhalil00 <talha.khalil707@gmail.com>
2026-01-20 21:43:40 +0500

    Chapter 1 Done

---
+++ Chapter-1 (Getting Started)/learnings.py
@@ -0,0 +1,16 @@
+# print("Hello World!")
+
+"""Module is a code that is written by someone which we can use in our programs, like we can use build in modules to take means like harmonic etc"""
+"""We use Pip to install packages or modules in our programs like for examples we can use it to install Pip install pyjokes which is a library used to get jokes"""
+
+# import pyjokes
+# joke = pyjokes.get_joke()
+# print(joke)
+
+"""We can use python in the cmd as well by just typing python in the cmd and simply running the code we write, it uses REPL(Read evaluate print loop) to read the code, evaluate the code, print the code and loop the code"""
+
+"""Using comments"""
+"""We can you single and multiple comments to add comment in the code which are not execuatable use by developers to add info about the code"""
+"""Single line comment we use (Pound #) Symbol at the start of the code"""
+"""Mutliline comment we use triple double or single quotes like we are using from the start and currently"""
+
---
+++ Chapter-1 (Getting Started)/practiece set.py
@@ -0,0 +1,19 @@
+"""1. Write a program to print Twinkle twinkle little star poem in python."""
+# print("""Twinkle Twinkle, Little Star
+# How I wonder what you are
+# Up above the world so high
+# Like a diamond in the sky
+# Twinkle Twinkle Little Star
+# How I wonder what you are!""")
+
+"""2. Use REPL and print the table of 5 using it. """
+
+"""3. Install an external module and use it to perform an operation of your interest."""
+# import pyjokes
+# print(pyjokes.get_joke())
+
+"""4. Write a python program to print the contents of a directory using the os module. Search online for the function which does that. """
+# import os
+# print(os.listdir("."))
+
+"""5. Label the program written in problem 4 with comments. """
