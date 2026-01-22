74db1ce25523152e386fbd2a6a3d68bb0299ff55
TalhaKhalil00 <talha.khalil707@gmail.com>
2026-01-23 01:34:56 +0500

    Chapter 2 Started

---
+++ Chapter-2 (Variables and simple data types)/learnings.py
@@ -0,0 +1,61 @@
+"""Variable Diclaration"""
+# a = 10
+# b = 10
+# print(a + b)
+
+"""Data Types"""
+"""1. integers
+2. string
+3. boolean
+4. none
+5. floating point numbers
+"""
+"""Operators in python"""
+# a = 10
+# b = 30
+# print(a + b)
+# c = b - a
+# c = b * a
+# c = b / a
+# print(c)
+# a -= 90
+# a += 90
+# a *= 90
+
+# a = 5 > 10
+# a = 5 < 10
+# a = 5 == 5
+# a = 5 != 10
+# print(a)
+
+"""Or Operator"""
+# print("True or True is:", True or True)
+# print("True or False is:", True or False)
+# print("False or True is:", False or True)
+# print("False or False is:", True or True)
+
+"""And Operator"""
+# print("True and True is:", True and True)
+# print("True and False is:", True and False)
+# print("False and True is:", False and True)
+# print("False and False is:", True and True)
+
+"""Not Operator"""
+# print(not(True))
+# print(not(False))
+
+"""Type Function and Casting"""
+
+# a = 123
+# b = "string"
+# c = 1.23
+# d = None
+# e = True
+# flt = float(a)
+
+# print(type(flt))
+
+"""Inpu() Functiona"""
+
+a = input("Enter your Number: ")
+print("Your Number is: ", a)
---
+++ Chapter-2 (Variables and simple data types)/practice_set.py
@@ -0,0 +1,38 @@
+"""1. Write a python program to add two numbers."""
+# first_num = int(input("Enter your first number : "))
+# second_num = int(input("Enter your second number : "))
+# sum = first_num + second_num
+# print("Sum of two numbers :",sum)
+
+"""2. Write a python program to find remainder when a number is divided by a number."""
+"""I have used modulas operator to get reminder (%)"""
+# first_num = int(input("Enter your first number : "))
+# second_num = int(input("Enter your second number : "))
+# divid = first_num % second_num
+# print(first_num, "divided by", second_num, "gives remainder", divid)
+
+"""3. Check the type of variable assigned using input () function."""
+# inp = input("Enter you value : ")
+# print("Your entered value is of", type(inp), "Type.")
+
+"""4. Use comparison operator to find out whether ‘a’ given variable a is greater than
+‘b’ or not. Take a = 34 and b = 80
+"""
+# a = 34
+# b = 80
+
+# c = a > b
+
+# print("A is greater than B",c)
+
+"""5. Write a python program to find an average of two numbers entered by the user."""
+# first_num = int(input("Enter your first number : "))
+# second_num = int(input("Enter your second number : "))
+# avg = (first_num + second_num)/2
+# print(f"Average of first and second number is {avg}")
+
+"""6. Write a python program to calculate the square of a number entered by the user."""
+# num = int(input("Enter your number : "))
+# sqr = num * num
+# print(f"{num}^2 = {sqr}")
+
